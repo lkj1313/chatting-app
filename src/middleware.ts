445e39ec476af54c9c77752cb702da1db8765f5c
KJ <smm45108@gmail.com>
2024-07-08 22:37:53 +0900

    feat: HeaderComponent 분리, 미들웨어 쿠키 확인 및 리디렉션, LayoutComponent 수정, 동적 라우팅 구현

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,9 +7,6 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect("/loginpage");
   }
 
-  // 토큰을 검증하는 추가적인 로직을 수행할 수 있습니다.
-  // 예를 들어, 서버에서 토큰을 검증하고 결과에 따라 처리합니다.
-
   return NextResponse.next(); // 요청을 다음 핸들러로 전달합니다.
 }
 
