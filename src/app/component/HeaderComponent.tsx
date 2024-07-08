549ede8443b19468306df5f7d94e4db48bdec37a
KJ <smm45108@gmail.com>
2024-07-09 00:24:49 +0900

    1

--- src/app/component/HeaderComponent.tsx
+++ src/app/component/HeaderComponent.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"; // 클라이언트 컴포넌트로 지정
 import React from "react";
 import { useDispatch } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
--- src/app/component/layoutComponent/LayOutComponent.tsx
+++ src/app/component/layoutComponent/LayOutComponent.tsx
@@ -1,28 +1,9 @@
-"use client";
-import React, { useRef, useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-
-import { closeSidebar } from "@/app/store/uiSlice";
-
-import { RootState } from "@/app/store/store";
 import Sidebar from "@/app/component/layoutComponent/components/SidebarComponent";
 import HeaderComponent from "../HeaderComponent";
-import ChatRoomList from "@/app/component/layoutComponent/components/ChatRoomList";
+
 import MainComponent from "./components/MainComponent";
 
 const LayoutComponent: React.FC = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const headerRef = useRef<HTMLDivElement>(null);
-
-  const [fadeIn, setFadeIn] = useState(false); // 추가된 상태
-  const dispatch = useDispatch();
-  const sidebarOpen = useSelector((state: RootState) => state.ui.sidebarOpen);
-  const showModal = useSelector((state: RootState) => state.ui.showModal);
-
-  useEffect(() => {
-    setFadeIn(true);
-  }, []);
-
   return (
     <div className="container">
       <HeaderComponent />
