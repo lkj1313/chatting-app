8420c80b996fb058e1182ab06db15445d58845d0
unknown <smm45108@gmail.com>
2024-07-29 19:33:43 +0900

    모바일 브라우저의 뷰포트 높이 문제 해결을 위한 useViewportHeight 훅 추가

--- src/app/privatechatroompage/[id]/page.tsx
+++ src/app/privatechatroompage/[id]/page.tsx
@@ -4,9 +4,14 @@ import PrivateChatRoomHeader from "./component/PrivateChatRoomHeader";
 import PrivateChatRoomPageMain from "./component/PrivateChatRoomMain";
 import PrivateChatRoomPageFooter from "./component/PrivateChatRoomFooter";
 import Sidebar from "./component/Sidebar";
+import useViewportHeight from "@/app/component/useViewportHeight";
 const PrivateChatRoomPage = () => {
+  useViewportHeight();
   return (
-    <div className="chat_wrap" style={{ position: "relative" }}>
+    <div
+      className="chat_wrap"
+      style={{ position: "relative", height: "calc(var(--vh) * 100)" }}
+    >
       <PrivateChatRoomHeader />
       <PrivateChatRoomPageMain />
       <PrivateChatRoomPageFooter />
